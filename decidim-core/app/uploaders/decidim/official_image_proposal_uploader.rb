da527eaa67120f4a0a06ad56304b9f497487ece2
Hilfe <daniel.hilfe@gmail.com>
2017-04-12 22:27:02 +0200

    Add image to official proposals

--- decidim-core/app/uploaders/decidim/official_image_proposal_uploader.rb
+++ decidim-core/app/uploaders/decidim/official_image_proposal_uploader.rb
@@ -2,6 +2,6 @@
 module Decidim
   # This class deals with uploading official images to Proposals.
   class OfficialImageProposalUploader < ImageUploader
-    process resize_to_limit: [300, 600]
+    process resize_to_limit: [1000, 1000]
   end
 end
